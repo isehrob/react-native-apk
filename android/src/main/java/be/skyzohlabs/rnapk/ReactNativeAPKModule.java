c1e45c35375e2548750d9a65336502b218307ddc
Sehrob <isehrob@gmail.com>
2019-07-13 22:09:35 +0500

    Update ReactNativeAPKModule.java
    Add WritableNativeArray instead of ArrayList

--- android/src/main/java/be/skyzohlabs/rnapk/ReactNativeAPKModule.java
+++ android/src/main/java/be/skyzohlabs/rnapk/ReactNativeAPKModule.java
@@ -13,6 +13,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableNativeArray;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -90,9 +91,9 @@ public class ReactNativeAPKModule extends ReactContextBaseJavaModule {
   public void getApps(Callback cb) {
     List<PackageInfo> packages = this.reactContext.getPackageManager().getInstalledPackages(0);
 
-    List<String> ret = new ArrayList<>();
+    WritableArray<String> ret = new WritableNativeArray<>();
     for (final PackageInfo p : packages) {
-      ret.add(p.packageName);
+      ret.pushString(p.packageName);
     }
     cb.invoke(ret);
   }
@@ -101,10 +102,10 @@ public class ReactNativeAPKModule extends ReactContextBaseJavaModule {
   public void getNonSystemApps(Callback cb) {
     List<PackageInfo> packages = this.reactContext.getPackageManager().getInstalledPackages(0);
 
-    List<String> ret = new ArrayList<>();
+    WritableArray<String> ret = new WritableNativeArray<>();
     for (final PackageInfo p : packages) {
       if ((p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-        ret.add(p.packageName);
+        ret.pushString(p.packageName);
       }
     }
     cb.invoke(ret);
